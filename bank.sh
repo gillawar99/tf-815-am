24680adc2a3438d7f266ca7c3f932517be11f017
Rahul Gillawar <145597075+gillawar99@users.noreply.github.com>
2023-12-11 12:22:05 +0530

    Update and rename ecomm.sh to bank.sh

--- bank.sh
+++ bank.sh
@@ -2,4 +2,4 @@
 sudo apt update -y
 sudo apt install -y nginx
 sudo rm /var/www/html/*.html
-sudo git clone https://github.com/ravi2krishna/ecomm.git /var/www/html
+sudo git clone https://github.com/ravi2krishna/bank.git /var/www/html
